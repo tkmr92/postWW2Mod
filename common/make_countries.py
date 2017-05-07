6401bd7d9bc72b42fbc2c256fc2ec5dd4aa028f2
tkmr <tkmr@kilo-tango.net>
2017-05-07 17:35:34 -0600

    Added script to automatically create an empty .txt file in /countries for countries listed in country_tag/00_countries.txt

---
+++ common/make_countries.py
@@ -0,0 +1,8 @@
+import re
+
+files = []
+
+with open('./country_tags/00_countries.txt', 'r') as FILE:
+    for line in FILE.readlines():
+        file = re.search('countries\/\w*\s*\w*.txt', line)
+        open(file.group(), 'a').close()
