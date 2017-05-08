2ff67ef7e3aa3a66e662034290fc0ca9e6adb0cd
tkmr <tkmr@kilo-tango.net>
2017-05-07 21:28:08 -0600

    ignore comments in 00_countries.txt

--- common/make_countries.py
+++ common/make_countries.py
@@ -4,5 +4,7 @@ files = []
 
 with open('./country_tags/00_countries.txt', 'r') as FILE:
     for line in FILE.readlines():
+        if "#" in line:
+            continue
         file = re.search('countries\/\w*\s*\w*.txt', line)
         open(file.group(), 'a').close()
